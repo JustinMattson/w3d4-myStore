83c3f7f2b8281096f50f29ae5fd408b9dc545e3d
Justin Mattson <justin.mattson@outlook.com>
2020-05-14 15:56:53 -0600

    updated store and chopsticks model with template

--- app/Models/ChopsticksModel.js
+++ app/Models/ChopsticksModel.js
@@ -1,12 +1,45 @@
+import { generateId } from "../Utils.js";
+
+console.log("hello from Chopsticks Model");
 export default class ChopsticksModel {
   constructor(data) {
-    // this.title = data.title;
-    // this.price = data.price;
-    // this.imgUrl = data.imgUrl;
-    console.log("hello from Chopsticks Model");
+    this.id = data.id || generateId();
+    this.title = data.title;
+    this.description = data.description;
+    this.price = data.price;
+    this.imgUrl = data.imgUrl;
   }
 
-  // get Template() {
-  //   return this.title;
-  // }
+  get Template() {
+    return /*html*/ `
+    <!-- ITEM TEMPATE BEGIN -->
+    <div class="col-3 d-flex justify-content-center mt-3">
+    <div class="card shadow">
+      <img
+        id="itemImageUrl"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSK1vM06QaNrJH8NsykCaEPPW5Lxwt9MsoprqxstVGbpi-VPWjF&usqp=CAU"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 id="itemTitle" class="card-title">
+          ${this.title}
+        </h5>
+        <p id="itemDescription" class="card-text">
+          ${this.description}
+        </p>
+        <h5 id="itemPrice" class="card-title">
+        $${this.price}
+        </h5>
+        <button
+          class="btn btn-outline-primary"
+          onclick="apps.chopsticksController.toCart(${this.id})"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  </div>
+  <!-- ITEM TEMPLATE END -->`;
+  }
 }
--- app/store.js
+++ app/store.js
@@ -2,7 +2,18 @@ import ChopsticksModel from "./Models/ChopsticksModel.js";
 
 let _state = {
   /** @type {ChopsticksModel[]} */
-  chopsticks: [],
+  chopsticks: [
+    /** @type {ChopsticksModel[] */
+    new ChopsticksModel({
+      id: "",
+      title: "Uni Japanese Chopstick Set - White",
+      description:
+        "Enjoy your favourite Asian delicacies with this Uni Japanese chopstick set from Christofle. Crafted from wood with silver plated bases.",
+      price: 81.05,
+      imgUrl:
+        "https://www.amara.com/static/uploads/images-2/products/huge/149550/uni-japanese-chopstick-set-white-480758.jpg",
+    }),
+  ],
   purchases: [],
 };
 

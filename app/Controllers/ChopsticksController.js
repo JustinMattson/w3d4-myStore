6ecfc83fa29df90e89c4066b697b0a2e6de88653
Justin Mattson <justin.mattson@outlook.com>
2020-05-14 14:32:21 -0600

    time to wire up the purchases.js

--- app/Controllers/ChopsticksController.js
+++
@@ -1,8 +0,0 @@
-import ChopsticksService from "../Services/ChopsticksService.js";
-
-//Public
-export default class ChopsticksController {
-  constructor() {
-    console.log("hello from Chopsticks controller");
-  }
-}

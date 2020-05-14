1cdb9d80a8c63ca426081a7539e766dde14cb4f6
Justin Mattson <justin.mattson@outlook.com>
2020-05-14 16:03:42 -0600

    updated with draw to chopsticks controller.

--- app/Controllers/ChopsticksController.js
+++ app/Controllers/ChopsticksController.js
@@ -1,8 +1,17 @@
 import ChopsticksService from "../Services/ChopsticksService.js";
+import _store from "../store.js";
+
+function _draw() {
+  let chopsticks = _store.State.chopsticks;
+  let template = "";
+  chopsticks.forEach((c) => (template += c.Template));
+  document.getElementById("chopsticks").innerHTML = template;
+}
 
 //Public
 export default class ChopsticksController {
   constructor() {
+    _draw();
     console.log("hello from chopsticks controller");
   }
 }

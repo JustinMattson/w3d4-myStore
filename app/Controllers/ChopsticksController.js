0a20f078c2ff1313f546bbe891c25fffa79a1336
Justin Mattson <justin.mattson@outlook.com>
2020-05-14 15:25:27 -0600

    updated chopsticks controller and main

---
+++ app/Controllers/ChopsticksController.js
@@ -0,0 +1,8 @@
+import ChopsticksService from "../Services/ChopsticksService.js";
+
+//Public
+export default class ChopsticksController {
+  constructor() {
+    console.log("hello from chopsticks controller");
+  }
+}
--- app/main.js
+++ app/main.js
@@ -1,17 +1,13 @@
-//import ChopsticksController from "./Controllers/ChopsticksController.js";
+import ChopsticksController from "./Controllers/ChopsticksController.js";
 
 import PurchaseController from "./Controllers/PurchaseController.js";
 
 class App {
-
   constructor() {
     console.log("hello from main.js");
-    //chopsticksController = new ChopsticksController();
-    this.purchaseController = new PurchaseController()
+    this.chopsticksController = new ChopsticksController();
+    this.purchaseController = new PurchaseController();
   }
-
-
 }
-
 
 window["app"] = new App();
